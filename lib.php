f37b840bbadd1de14a87db8df20b99b4f04ecf9d
Robert Russo <rrusso@lsu.edu>
2022-08-19 14:59:56 +0000

    make sure the auth string is lowercase

--- lib.php
+++ lib.php
@@ -694,7 +694,7 @@ class lsuxe_helpers {
         $luser->firstname     = $user->firstname;
         $luser->lastname      = $user->lastname;
         $luser->alternatename = $user->alternatename;
-        $luser->auth          = $user->xauth;
+        $luser->auth          = strtolower($user->xauth);
 
         if ($luser == $ruser) {
             mtrace("The local ($luser->username) and remote user ($ruser->username) objects match entirely. Skipping.");
@@ -730,7 +730,7 @@ class lsuxe_helpers {
             'users[0][id]' => $returneduser['id'],
             'users[0][username]' => $user->username,
             'users[0][email]' => $user->email,
-            'users[0][auth]' => $user->xauth,
+            'users[0][auth]' => strtolower($user->xauth),
             'users[0][firstname]' => $user->firstname,
             'users[0][lastname]' => $user->lastname,
             'users[0][alternatename]' => $user->alternatename,
@@ -792,7 +792,7 @@ class lsuxe_helpers {
             'moodlewsrestformat' => 'json',
             'users[0][username]' => $user->username,
             'users[0][email]' => $user->email,
-            'users[0][auth]' => $user->xauth,
+            'users[0][auth]' => strtolower($user->xauth),
             'users[0][firstname]' => $user->firstname,
             'users[0][lastname]' => $user->lastname,
             'users[0][alternatename]' => $user->alternatename,
